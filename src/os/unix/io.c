29a2deb1f3ca1fc934ce2798f02764545611db3a
Godzil <godzil@godzil.net>
2018-01-30 16:18:12 +0000

    Not sure what was done here before.

--- src/os/unix/io.c
+++ src/os/unix/io.c
@@ -46,7 +46,7 @@ int console_printf(const ConsoleLevel level, const char *format, ...)
    console_vprintf(level, format, ap);
 
    va_end(ap);
-   return ret;
+   return 0;
 }
 
 int console_printf_d(const char *format, ...)
